ac698cc448c0d5bae6862b6bf38571893839d5bb
NicHub <nicolas.jeanmonod@gmail.com>
2023-08-04 19:36:34 +0200

    update

--- product_test.py
+++ product_test.py
@@ -10,8 +10,13 @@ from functools import reduce
 from itertools import accumulate
 from operator import mul
 
-import numpy
+import numpy as np
 import perfplot
+
+
+def mathexp_prod(lst):
+    ans = math.exp(sum(map(math.log, lst)))
+    return ans
 
 
 def reduce_lambda_prod(lst):
@@ -25,14 +30,14 @@ def reduce_mul_prod(lst):
 
 
 def forloop_prod(lst):
-    ans = 1
-    for elem in lst:
+    ans = lst[0]
+    for elem in lst[1:]:
         ans *= elem
     return ans
 
 
 def numpy_prod(lst):
-    ans = numpy.prod(lst)
+    ans = np.prod(lst)
     return ans
 
 
@@ -41,46 +46,58 @@ def math_prod(lst):
     return ans
 
 
-def itertools_accumulate_1_prod(lst):
+def itertools_accumulate_prod_1(lst):
     for ans in accumulate(lst, mul):
         pass
     return ans
 
 
-def itertools_accumulate_2_prod(lst):
-    ans = list(accumulate(lst, lambda x, y: x * y))[-1]
+def itertools_accumulate_prod_2(lst):
+    ans = [_ for _ in accumulate(lst, mul)][-1]
     return ans
 
 
 def while_prod(lst):
-    ans = 1
-    i = 0
+    ans = lst[0]
+    i = 1
     while i < len(lst):
         ans *= lst[i]
         i += 1
     return ans
 
 
-def lambda_prod(lst):
+def lambda_prod_1(lst):
     ans = [j := (lambda c, i: i * j if c else i)(c, i) for c, i in enumerate(lst)][-1]
     return ans
 
 
-b = perfplot.bench(
-    setup=numpy.random.rand,
-    kernels=[
-        lambda_prod,
-        while_prod,
-        itertools_accumulate_2_prod,
-        reduce_lambda_prod,
-        itertools_accumulate_1_prod,
-        forloop_prod,
-        reduce_mul_prod,
-        math_prod,
-        numpy_prod,
-    ],
-    n_range=[10**k for k in range(0, 5)],
-    xlabel="len(a)",
-)
-b.save("out.png")
-# b.show()
+def lambda_prod_2(lst):
+    if len(lst) == 1:
+        ans = lst[0]
+    else:
+        j = lst[0]
+        ans = [j := (lambda i: i * j)(i) for i in lst[1:]][-1]
+    return ans
+
+
+if __name__ == "__main__":
+    b = perfplot.bench(
+        setup=np.random.rand,
+        kernels=[
+            lambda_prod_1,
+            lambda_prod_2,
+            while_prod,
+            reduce_lambda_prod,
+            itertools_accumulate_prod_2,
+            mathexp_prod,
+            itertools_accumulate_prod_1,
+            forloop_prod,
+            reduce_mul_prod,
+            math_prod,
+            numpy_prod,
+        ],
+        n_range=[10**k for k in range(0, 5)],
+        xlabel="len(a)",
+    )
+    b.save("out.png")
+    # b.show()
